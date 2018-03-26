23cd1ed7fc3e9ffbabc1a0cd99c76cfe934132d0
AuCson <2474010692@qq.com>
2018-03-27 01:24:18 +0800

    bug fix

--- verbal/views.py
+++ verbal/views.py
@@ -12,39 +12,36 @@ class VerbalView(View):
             self.data = json.load(f)
 
     def get(self, request, *args, **kwargs):
-        if 'lid' in request.GET:
-            lid = request.GET['lid']
-            try:
-                if 'uid' in request.GET:
-                    uid = request.GET['uid']
-                    word_list = self.data[lid][uid]
-                else:
-                    word_list = []
-                    for unit in sorted(self.data[lid],key=lambda x:int(x)):
-                        word_list.extend(self.data[lid][unit])
-            except KeyError:
-                return HttpResponseNotFound()
-            render_list = []
-            cnt = 0
-            for word_data in word_list:
-                for word in word_data:
-                    usages = word_data[word]
-                    for usage in usages:
-                        if 'eg' in usage and 'paraphrase' in usage:
-                            flat_eg = ''
-                            for eg in usage['eg']:
-                                flat_eg += eg['eng'] +'\t' + eg['cn'] + '\n'
-                            flat_eg = flat_eg.replace(word, '[____]')
-                            flat_eg = flat_eg.replace(word.capitalize(), '[____]')
-                            render_list.append({
-                                'eg': flat_eg,
-                                'word': word,
-                                'paraphrase': usage['paraphrase'],
-                                'hash':word + str(cnt)
-                            })
-                            cnt += 1
-            list_id = lid
-            unit_id = request.GET['uid'] if 'uid' in request.GET else 'All'
-            return render(request, 'verbal.html', {'verbal_list':render_list, 'list_id':list_id, 'unit_id':unit_id})
-        else:
+        lid = request.GET.get('lid',1)
+        try:
+            if 'uid' in request.GET:
+                uid = request.GET['uid']
+                word_list = self.data[lid][uid]
+            else:
+                word_list = []
+                for unit in sorted(self.data[lid],key=lambda x:int(x)):
+                    word_list.extend(self.data[lid][unit])
+        except KeyError:
             return HttpResponseNotFound()
+        render_list = []
+        cnt = 0
+        for word_data in word_list:
+            for word in word_data:
+                usages = word_data[word]
+                for usage in usages:
+                    if 'eg' in usage and 'paraphrase' in usage:
+                        flat_eg = ''
+                        for eg in usage['eg']:
+                            flat_eg += eg['eng'] +'\t' + eg['cn'] + '\n'
+                        flat_eg = flat_eg.replace(word, '[____]')
+                        flat_eg = flat_eg.replace(word.capitalize(), '[____]')
+                        render_list.append({
+                            'eg': flat_eg,
+                            'word': word,
+                            'paraphrase': usage['paraphrase'],
+                            'hash':word + str(cnt)
+                        })
+                        cnt += 1
+        list_id = lid
+        unit_id = request.GET['uid'] if 'uid' in request.GET else 'All'
+        return render(request, 'verbal.html', {'verbal_list':render_list, 'list_id':list_id, 'unit_id':unit_id})
